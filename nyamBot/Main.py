dfcebd35a5a9a1925bb75c46708fb6cae654f28e
Summer <coconamy7727@naver.com>
2019-07-12 11:25:10 +0900

    선택 부분 구현

--- nyamBot/Main.py
+++ nyamBot/Main.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import random
 import re
 import urllib.request
 from string import punctuation
@@ -11,6 +12,7 @@ from slack.web.classes import extract_json
 from slack.web.classes.blocks import ImageBlock, SectionBlock
 from slackeventsapi import SlackEventAdapter
 
+from nyamBot.bob_worldcup import worldcup
 from nyamBot.first_recommendation import first_recom
 
 SLACK_TOKEN = 'xoxb-684597766369-679535522738-KefPJIzqi0EDCa4PHW6UuTBT'
@@ -25,9 +27,10 @@ slack_web_client = WebClient(token=SLACK_TOKEN)
 
 # 크롤링 함수 구현하기
 def main_question(text):
-
+    blocks=[]
     if '냠냠' in text or '~다시추천' in text:
         food_list = []
+
         food_line = ""
         with open("food_list.txt", 'rt', encoding='UTF8') as food_file:
             for line in food_file:
@@ -35,17 +38,17 @@ def main_question(text):
             food_list=food_line.strip().split(",")[:-1]
         food = food_list[random.randrange(1, len(food_list))]
         block1 = ImageBlock(image_url="https://mp-seoul-image-production-s3.mangoplate.com/46971_1429020571192?fit=around|359:240&crop=359:240;*,*&output-format=jpg&output-quality=80",alt_text="이미지가 안뜬다냠... ㅠㅠ미안하다냠...")
-        block2 = SectionBlock(text = "오늘은 여기서 "+food+" 어떠냠? 별로면 `~다시추천`, 원하는 메뉴 식당 알고 싶으면 `~메뉴 <메뉴이름> <위치>`, "
-                                     "고르고 싶다면 `~선택`이라고 입력해주겠냠")
-        return [block1, block2]
+        block2 = SectionBlock(text = "오늘은 여기서 *"+food+"* 어떠냠? 별로면 `~다시추천`, 원하는 메뉴 식당 알고 싶으면 `~메뉴 <메뉴이름> <위치>`, "
+                                     ", 너도 모르는 너가 원하는 메뉴를 알고 싶다면 `!이상밥월드컵`, 키워드로 고르고 싶다면 `~선택`이라고 입력해주겠냠")
+        blocks = [block1, block2]
 
-    # elif '~다시추천' in text:
-    #     app_mentioned('냠냠')
+    elif '!이상밥월드컵' in text:
+        blocks = worldcup(text)
 
     else:
         blocks = first_recom(text)
 
-        return blocks
+    return blocks
 
 
 @slack_events_adaptor.on("app_mention")
@@ -53,7 +56,7 @@ def app_mentioned(event_data):
     channel = event_data["event"]["channel"]
     text = event_data["event"]["text"]
     message_blocks = []
-    if not '냠냠' in text and not '~' in text:
+    if not '냠냠' in text and not '~' in text and not '!이상밥월드컵' in text:
         slack_web_client.chat_postMessage(
             channel=channel,
             text="`@<봇이름> 냠냠`으로 시작하면 안되냠. 이유는 없다 그냥 귀엽지않냠"
---
+++ nyamBot/bob_worldcup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import random
+import re
+import urllib.request
+from string import punctuation
+
+from bs4 import BeautifulSoup
+
+from flask import Flask
+from slack import WebClient
+from slack.web.classes import extract_json
+from slack.web.classes.blocks import ImageBlock, SectionBlock
+from slackeventsapi import SlackEventAdapter
+
+
+
+
+
+def worldcup(text):
+    blocks = []
+    if '이상밥월드컵' in text:
+        food_and_location = text.split()[2:]
+        blocks.append(SectionBlock(text="오늘은 이 메뉴 어떠냠 주변 식당 알고 싶으면 `~<메뉴이름> <위치>`, 별로면 `~다시추천`, "
+                                   "고르고 싶다면 `~선택`이라고 입력해보겠냠"))
+    else:
+        block = SectionBlock(text="선택을 선택했구냠!! 너의 취향에 따라 메뉴를 추천해줄테니 골라보겠느냠!\n"
+                                   "먼저 기준을 골라보겠냠?\n"
+                                   "`~날씨` : 오늘 날씨에 딱 맞는 메뉴를 추천해주겠다냠!\n"
+                                   "`~분위기` : 예쁜, 가성비, 깔끔 등 다양한 키워드로 특별한 날을 기념해 보는건 어떠냠?\n"
+                                   "`~방문목적` 데이트, 회식, 혼술, 가족외식 등 목적에 따라 추천도 해주겠다냠!!\n"
+                                   "`~편의시설` 강아지, 주차, 24시간 등 특별한 상황도 추천해주겠다냠!!\n"
+                                   "`~인싸맛집` : 방송에 나온 핫한 식당을 가서 핵인싸로 거듭나는건 어떠냠!!\n"
+                                   "위에 코드 중 하나를 선택해서 설명을 들어라냠!")
+        blocks.append(block)
+
+
+    return blocks
